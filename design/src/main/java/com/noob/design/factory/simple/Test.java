9d21a9a49e382db0cd2123a092ff9aeb387a92f6
huangliang <hl7226261@2980.com>
2019-03-08 23:04:18 +0800

    简单工厂模式

--- design/src/main/java/com/noob/design/factory/simple/Test.java
+++ design/src/main/java/com/noob/design/factory/simple/Test.java
@@ -1,12 +1,12 @@
 package com.noob.design.factory.simple;
 
-import com.noob.design.factory.App;
+import com.noob.design.factory.Apple;
 import com.noob.design.factory.IFruit;
 
 public class Test {
     public static void main(String[] args) {
         SimpleFactory factory = new SimpleFactory();
-        IFruit app = factory.create(App.class);
+        IFruit app = factory.create(Apple.class);
         app.eat();
     }
 }
