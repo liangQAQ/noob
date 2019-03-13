c2689346ee21b3008883acba58beaf81e50f8d85
huangliang <hl7226261@2980.com>
2019-03-13 23:04:53 +0800

    工厂模式增加注释说明

--- design/src/main/java/com/noob/design/factory/simple/SimpleFactory.java
+++ design/src/main/java/com/noob/design/factory/simple/SimpleFactory.java
@@ -2,6 +2,19 @@ package com.noob.design.factory.simple;
 
 import com.noob.design.factory.IFruit;
 
+/**
+ * ## 简单工厂模式
+ * 是指由一个工厂对象决定创建出哪一种产品类的实例。
+ * ### 适用场景
+ * 工厂类负责创建的对象较少。
+ * 客户端只需要传入工厂类的参数，对于如何创建对象的逻辑不需要关心。
+ * ### 优点
+ * 只需传入一个正确的参数，就可以获取你所需要的对象
+ * 无须知道其创建的细节。
+ * ### 缺点
+ * 工厂类的职责相对过重，增加新的产品时需要修改工厂类的判断逻辑，违背开闭原则。
+ * 不易于扩展过于复杂的产品结构。
+ */
 public class SimpleFactory {
 
     public IFruit create(Class<? extends IFruit> clazz){
