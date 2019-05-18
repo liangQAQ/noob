9fcafef4d868f95153c5042b914c601eaeb21202
huangliang <hl7226261@2980.com>
2019-05-18 17:31:41 +0800

    原型模式相关

---
+++ design/src/main/java/com/noob/design/clone/Home.java
@@ -0,0 +1,20 @@
+package com.noob.design.clone;
+
+public class Home {
+    private String address;
+
+    public Home(String address) {
+        this.address = address;
+    }
+
+    public Home() {
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
