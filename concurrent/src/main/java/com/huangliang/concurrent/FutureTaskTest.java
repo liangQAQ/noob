b1ac05e17fb2ee6386484397b1913fdaac08ba3c
huangliang <hl7226261@2980.com>
2019-06-06 14:49:56 +0800

    futureTask异步返回结果测试

---
+++ concurrent/src/main/java/com/huangliang/concurrent/FutureTaskTest.java
@@ -0,0 +1,21 @@
+package com.huangliang.concurrent;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class FutureTaskTest {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        FutureTask ft = new FutureTask(new call());
+        new Thread(ft).start();
+        System.out.println(ft.get());
+    }
+
+    static class call implements Callable{
+        public String call() throws Exception {
+            Thread.sleep(3000);
+            return "calllll";
+        }
+    }
+}
